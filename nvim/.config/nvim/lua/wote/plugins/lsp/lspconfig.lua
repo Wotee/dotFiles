81aa83bccb9c4d4da58250cbe2554d7002be1872
Valtteri Tarvus <valtteri.tarvus@if.fi>
2025-02-03 10:32:54 +0200

    Use vim instead of fzf-lua in gd

--- nvim/.config/nvim/lua/wote/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/wote/plugins/lsp/lspconfig.lua
@@ -35,8 +35,7 @@ return {
 				keymap.set("n", "gD", "<cmd>FzfLua lsp_declarations<CR>", opts) -- go to declaration
 
 				opts.desc = "Show LSP definitions"
-				-- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-				keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", opts) -- go to declaration
+				keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
 
 				opts.desc = "Show LSP implementations"
 				-- keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
