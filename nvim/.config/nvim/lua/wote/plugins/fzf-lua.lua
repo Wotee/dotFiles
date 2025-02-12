3169ed58e99e60030e6cfca47f66cf825f89148a
Valtteri Tarvus <valtteri.tarvus@if.fi>
2025-02-12 07:48:19 +0200

    Mini.nvim to replace some others

--- nvim/.config/nvim/lua/wote/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/wote/plugins/fzf-lua.lua
@@ -8,7 +8,10 @@ return {
 
 		keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fs", "<cmd>FzfLua live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fw", "<cmd>FzfLua grep_cword<cr>", { desc = "Find word under cursor" })
+		keymap.set("n", "<leader>fW", "<cmd>FzfLua grep_cWORD<cr>", { desc = "Find WORD under cursor" })
 		keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Find buffers" })
 		keymap.set("n", "<leader>fr", "<cmd>FzfLua lsp_references<cr>", { desc = "Find references" })
+		keymap.set("n", "<leader>fp", "<cmd>FzfLua resume<cr>", { desc = "Resume work where you left off" })
 	end,
 }
---
+++ nvim/.config/nvim/lua/wote/plugins/mini-nvim.lua
@@ -0,0 +1,7 @@
+return {
+	"echasnovski/mini.nvim",
+	version = false, -- Use main branch
+	setup = function()
+		require("mini.nvim").setup()
+	end,
+}
