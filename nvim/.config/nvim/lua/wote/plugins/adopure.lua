73b7469f20f4d0a28b51d0b9cb7ff858bc9f592b
Valtteri Tarvus <valtteri.tarvus@if.fi>
2025-02-16 07:45:12 +0200

    Improve startup

--- nvim/.config/nvim/lua/wote/plugins/adopure.lua
+++ nvim/.config/nvim/lua/wote/plugins/adopure.lua
@@ -4,25 +4,17 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-lua/telescope.nvim",
 	},
-	config = function()
-		vim.g.adopure = {}
-
-		local function set_keymap(keymap, command)
-			vim.keymap.set({ "n", "v" }, keymap, function()
-				vim.cmd(":" .. command)
-			end, { desc = command })
-		end
-
-		set_keymap("<leader>alc", "Adopure load context")
-		set_keymap("<leader>alt", "Adopure load threads")
-		set_keymap("<leader>aoq", "AdoPure open quickfix")
-		set_keymap("<leader>aot", "AdoPure open thread_picker")
-		set_keymap("<leader>aon", "AdoPure open new_thread")
-		set_keymap("<leader>aoe", "AdoPure open existing_thread")
-		set_keymap("<leader>asc", "AdoPure submit comment")
-		set_keymap("<leader>asv", "AdoPure submit vote")
-		set_keymap("<leader>ast", "AdoPure submit thread_status")
-		set_keymap("<leader>asd", "AdoPure submit delete_comment")
-		set_keymap("<leader>ase", "AdoPure submit edit_comment")
-	end,
+	keys = {
+		{ "<leader>alc", "<cmd>Adopure load context<CR>", desc = "Load context" },
+		{ "<leader>alt", "<cmd>Adopure load threads<CR>", desc = "Load threads" },
+		{ "<leader>aoq", "<cmd>AdoPure open quickfix<CR>", desc = "Open quickfix" },
+		{ "<leader>aot", "<cmd>AdoPure open thread_picker<CR>", desc = "Thread picker" },
+		{ "<leader>aon", "<cmd>AdoPure open new_thread<CR>", desc = "Open new thread" },
+		{ "<leader>aoe", "<cmd>AdoPure open existing_thread<CR>", desc = "Open existing thread" },
+		{ "<leader>asc", "<cmd>AdoPure submit comment<CR>", desc = "Submit comment" },
+		{ "<leader>asv", "<cmd>AdoPure submit vote<CR>", desc = "Submit vote" },
+		{ "<leader>ast", "<cmd>AdoPure submit thread_status<CR>", desc = "Submit thread status" },
+		{ "<leader>asd", "<cmd>AdoPure submit delete_comment<CR>", desc = "Submit delete comment" },
+		{ "<leader>ase", "<cmd>AdoPure submit edit_comment<CR>", desc = "Submit edit comment" },
+	},
 }
--- nvim/.config/nvim/lua/wote/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/wote/plugins/fzf-lua.lua
@@ -1,17 +1,13 @@
 return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("fzf-lua").setup({})
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fs", "<cmd>FzfLua live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fw", "<cmd>FzfLua grep_cword<cr>", { desc = "Find word under cursor" })
-		keymap.set("n", "<leader>fW", "<cmd>FzfLua grep_cWORD<cr>", { desc = "Find WORD under cursor" })
-		keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Find buffers" })
-		keymap.set("n", "<leader>fr", "<cmd>FzfLua lsp_references<cr>", { desc = "Find references" })
-		keymap.set("n", "<leader>fp", "<cmd>FzfLua resume<cr>", { desc = "Resume work where you left off" })
-	end,
+	keys = {
+		{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Fuzzy find files in cwd" },
+		{ "<leader>fs", "<cmd>FzfLua live_grep<cr>", desc = "Find string in cwd" },
+		{ "<leader>fw", "<cmd>FzfLua grep_cword<cr>", desc = "Find word under cursor" },
+		{ "<leader>fW", "<cmd>FzfLua grep_cWORD<cr>", desc = "Find WORD under cursor" },
+		{ "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Find buffers" },
+		{ "<leader>fr", "<cmd>FzfLua lsp_references<cr>", desc = "Find references" },
+		{ "<leader>fp", "<cmd>FzfLua resume<cr>", desc = "Resume work where you left off" },
+	},
 }
