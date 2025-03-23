66e3b9e4971dbd6eea3f0eb345fdebec238143fb
Valtteri Tarvus <valtteri.tarvus@if.fi>
2025-03-23 06:47:34 +0200

    Close other buffors keymap

--- nvim/.config/nvim/lua/wote/core/keymaps.lua
+++ nvim/.config/nvim/lua/wote/core/keymaps.lua
@@ -24,3 +24,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- buffer management
+keymap.set("n", "<leader>bo", "<cmd>%bd|e#|bd#<CR>", { desc = "Close other buffers" })
