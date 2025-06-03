fc68c92001b19a86624f236a4d67d03421dc0692
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2025-06-03 13:03:56 +0300

    Add MCPHub, integrate it with blink and codecompanion

--- nvim/.config/nvim/lua/wote/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/wote/plugins/blink-cmp.lua
@@ -1,13 +1,16 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"olimorris/codecompanion.nvim", -- For enabling code companion completion sources
+	},
 	version = "1.*",
 	opts = {
 		keymap = { preset = "default" },
 		appearance = { nerd_font_variant = "mono" },
 		completion = { documentation = { auto_show = false } },
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path", "snippets", "buffer", "codecompanion" },
 		},
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 		cmdline = {
--- nvim/.config/nvim/lua/wote/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/wote/plugins/codecompanion.lua
@@ -1,6 +1,17 @@
 return {
 	"olimorris/codecompanion.nvim",
-	opts = {},
+	opts = {
+		extensions = {
+			mcphub = {
+				callback = "mcphub.extensions.codecompanion",
+				opts = {
+					show_results_in_chat = true, -- Show mcp tool results in chat
+					make_vars = true, -- Convert resources to #variables
+					make_slash_commands = true, -- Add prompts as /slash commands
+				},
+			},
+		},
+	},
 	keys = {
 		{ "<leader>ct", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle CodeCompanion Chat" },
 	},
