9186e0b29cc4fbe6e9bc5e29bfb1bd17c988e498
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2025-06-14 11:14:45 +0300

    Don't automatically make zsh default

--- home-manager/home2.nix
+++ home-manager/home2.nix
@@ -31,42 +31,12 @@
   ];
 
   home.sessionVariables = {
-    GTK_THEME = "Adwaita:dark";
     EDITOR = "nvim";
     DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_9_0}/share/dotnet";
   };
 
-  # Set zsh as default shell on activation
   home.activation = {
-    make-zsh-default-shell = lib.hm.dag.entryAfter ["installPackages"] ''
-           # if zsh is not the current shell
-           PATH="/usr/bin:/bin:$PATH"
-           ZSH_PATH="/home/wote/.nix-profile/bin/zsh"
-           if [[ $(getent passwd wote) != *"$ZSH_PATH" ]]; then
-             echo "ZSH PATH: $ZSH_PATH"
-             if grep $ZSH_PATH /etc/shells; then
-               echo "The thing is there already"
-             else
-               echo "Adding zsh to /etc/shells. password might be necessary."
-               run echo "$ZSH_PATH" | sudo tee -a /etc/shells
-             fi
-             echo "setting zsh as default shell (using chsh). password might be necessary."
-             if grep $ZSH_PATH /etc/shells; then
-               echo "adding zsh to /etc/shells"
-             fi
-             echo "running chsh to make zsh the default shell"
-             run chsh -s $ZSH_PATH wote
-             echo "zsh is now set as default shell !"
-      echo "Launching zsh"
-      echo "##########################################"
-      echo "# Rest of the nix setup not run!         #"
-      echo "# Restart your shell and run             #"
-      echo "# \`nix run home-manager switch\` again!   #"
-      echo "##########################################"
-      $ZSH_PATH
-           fi
-    '';
-    nodeInstall = lib.hm.dag.entryAfter ["make-zsh-default-shell"] ''
+    nodeInstall = lib.hm.dag.entryAfter ["installPackages"] ''
       ${pkgs.fnm}/bin/fnm use --install-if-missing 22
     '';
   };
