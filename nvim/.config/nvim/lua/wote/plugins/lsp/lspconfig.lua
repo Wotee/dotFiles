5c41574a93ebbd66e364f42d9768e5561f00b7ff
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2025-08-20 14:12:31 +0300

    Install harper-ls

--- nvim/.config/nvim/lua/wote/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/wote/plugins/lsp/lspconfig.lua
@@ -30,6 +30,38 @@ return {
 
 		lspconfig.contextive.setup({})
 
+		-- lspconfig.harper_ls.setup({
+		-- 	filetypes = { "markdown", "text" },
+		-- 	settings = {
+		-- 		["harper-ls"] = {
+		-- 			linters = {
+		-- 				SpellCheck = true,
+		-- 				SpelledNumbers = false,
+		-- 				AnA = true,
+		-- 				SentenceCapitalization = true,
+		-- 				UnclosedQuotes = true,
+		-- 				WrongQuotes = false,
+		-- 				LongSentences = true,
+		-- 				RepeatedWords = true,
+		-- 				Spaces = true,
+		-- 				Matcher = true,
+		-- 				CorrectNumberSuffix = true,
+		-- 			},
+		-- 			codeActions = {
+		-- 				ForceStable = false,
+		-- 			},
+		-- 			markdown = {
+		-- 				IgnoreLinkTitle = false,
+		-- 			},
+		-- 			diagnosticSeverity = "hint",
+		-- 			isolateEnglish = false,
+		-- 			dialect = "American",
+		-- 			maxFileLength = 120000,
+		-- 			ignoredLintsPath = {},
+		-- 		},
+		-- 	},
+		-- })
+
 		local keymap = vim.keymap -- for conciseness
 
 		vim.api.nvim_create_autocmd("LspAttach", {
