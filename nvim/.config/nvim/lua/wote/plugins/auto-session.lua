98e808bd4c625ea2683e8cec276f66547a3db8dd
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2025-11-10 12:00:59 +0200

    Migrate from deprecated commands

--- nvim/.config/nvim/lua/wote/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/wote/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
 		auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
 	},
 	keys = {
-		{ "<leader>wr", "<cmd>SessionRestore<CR>", desc = "Restore session for cwd" }, -- restore last workspace session for current directory
-		{ "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session for auto session root dir" }, -- save workspace session for current working directory
+		{ "<leader>wr", "<cmd>AutoSession restore<CR>", desc = "Restore session for cwd" }, -- restore last workspace session for current directory
+		{ "<leader>ws", "<cmd>AutoSession save<CR>", desc = "Save session for auto session root dir" }, -- save workspace session for current working directory
 	},
 }
