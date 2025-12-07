9a20637bbcbe89e926d4517fd415de85cc9408e8
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2025-12-07 23:17:57 +0200

    Neotest status to lualine

--- nvim/.config/nvim/lua/wote/plugins/lualine.lua
+++ nvim/.config/nvim/lua/wote/plugins/lualine.lua
@@ -59,7 +59,24 @@ return {
 					},
 				},
 				-- Disable progress indicators
-				lualine_y = {},
+				lualine_y = {
+					{
+						function()
+							local neotest = require("neotest")
+							local adapter_ids = neotest.state.adapter_ids()
+							local adapter_id = adapter_ids[1]
+							local statuses = neotest.state.status_counts(adapter_id)
+							if statuses.running > 0 then
+								return ""
+							end
+							if statuses.failed > 0 then
+								return ""
+							else
+								return ""
+							end
+						end,
+					},
+				},
 			},
 		})
 	end,
