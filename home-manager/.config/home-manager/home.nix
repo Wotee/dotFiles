958cececd05d40a41865458c2402d343f17ce601
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2025-12-08 21:25:30 +0200

    Comment podman related stuff

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -51,10 +51,10 @@ in {
     pkgs.zoxide
     pkgs.zathura # Nix viewengine
     latex
-    pkgs.podman
-    pkgs.docker-client # Docker CLI, without Docker daemon
-    pkgs.docker-compose # Standalone docker compose v1
-    pkgs.podman-compose # Native podman-compose
+    # pkgs.podman
+    # pkgs.docker-client # Docker CLI, without Docker daemon
+    # pkgs.docker-compose # Standalone docker compose v1
+    # pkgs.podman-compose # Native podman-compose
     pkgs.lazydocker
     pkgs.obsidian
   ];
