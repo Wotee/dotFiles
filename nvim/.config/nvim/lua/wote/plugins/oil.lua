9e6d83e570611b0801afa7bfd10cb188d3ac11c3
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2025-12-28 19:32:47 +0200

    Migrate from nvim-tree to oil

---
+++ nvim/.config/nvim/lua/wote/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	keys = {
+		{ "<leader>ee", "<cmd>Oil . --float<CR>", desc = "Toggle file explorer" },
+	},
+}
