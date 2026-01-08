0d14be1345060ad6ce52d333e5032ab7cccd2d31
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2026-01-08 10:02:20 +0200

    Update title string

--- nvim/.config/nvim/lua/wote/core/options.lua
+++ nvim/.config/nvim/lua/wote/core/options.lua
@@ -2,6 +2,9 @@ vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt
 
+opt.title = true
+vim.opt.titlestring = "nvim [%{fnamemodify(getcwd(), ':t')}] %t"
+
 opt.relativenumber = true
 opt.number = true
 
