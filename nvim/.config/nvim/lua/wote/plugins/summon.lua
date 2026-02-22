452353c97b508d05f63bfcff328ba697bf54671d
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2026-02-22 18:43:55 +0200

    Add summon for faster todos

---
+++ nvim/.config/nvim/lua/wote/plugins/summon.lua
@@ -0,0 +1,13 @@
+return {
+	"salkhalil/summon.nvim",
+	opts = {
+		commands = {
+			todos = {
+				type = "file",
+				command = "~/git/vaults/work/todo.md",
+				title = " TODOs ",
+				keymap = "<leader>t",
+			},
+		},
+	},
+}
