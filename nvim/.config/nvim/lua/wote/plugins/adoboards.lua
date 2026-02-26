a099f37075b90fdcd5f271984d3f85333b9bf02c
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2026-02-26 13:51:17 +0200

    Update nvim plugins

--- nvim/.config/nvim/lua/wote/plugins/adoboards.lua
+++ nvim/.config/nvim/lua/wote/plugins/adoboards.lua
@@ -1,6 +1,6 @@
 return {
 	"Wotee/adoboards.nvim",
-	-- dir = "/home/wote/git/adoboards.nvim/",
+	-- dir = "/home/wote/git/adoboards.nvim",
 	opts = {},
 	keys = {
 		{ "<leader>ab", "<cmd>Adoboards<cr>", desc = "Open AdoBoards" },
