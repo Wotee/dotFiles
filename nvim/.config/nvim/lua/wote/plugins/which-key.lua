d8ef6bb03c867b81aa69fb3048ed22a57873f404
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2026-04-22 17:38:13 +0300

    Add some groups for which-key

--- nvim/.config/nvim/lua/wote/plugins/which-key.lua
+++ nvim/.config/nvim/lua/wote/plugins/which-key.lua
@@ -5,4 +5,16 @@ return {
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
 	end,
+	opts = {
+		spec = {
+			{ "<leader>f", group = "Find" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>c", group = "Code" },
+			{ "<leader>b", group = "Buffer" },
+			{ "<leader>r", group = "Run" },
+			{ "<leader>z", group = "Debug" },
+			{ "<leader>x", group = "Diagnostics" },
+			{ "gs", group = "Surround" },
+		},
+	},
 }
